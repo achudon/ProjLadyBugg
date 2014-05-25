1d13b57be59862d92e19ddd81fb1dcc7a717235a
Alyssa Hudon <piningpianist@gmail.com>
2014-05-25 14:06:06 -0700

    adding files again

---
+++ TicTacToe.js
@@ -0,0 +1,15 @@
+var turn = 0;
+
+var winCombos = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
+
+var squaresPlayed = [9];
+
+window.onload = initializeAll(); 
+
+alert('got here!');
+function initializeAll {
+	winCombos = new Array();
+	squaresPlayed = new Array();
+	console.log("hello");
+}
+
